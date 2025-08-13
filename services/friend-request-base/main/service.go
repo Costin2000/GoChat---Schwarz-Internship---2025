c0f94bb35b2f8ac91fe96f9bdf54806e9cd537b6
Alexandru Axenia <axeniaalex1@yahoo.com>
2025-08-14 02:37:48 +0300

    GI23-162 - Set Up friend-request-base

--- services/friend-request-base/main/service.go
+++ services/friend-request-base/main/service.go
@@ -1,5 +1,35 @@
-package pkg
+package main
+
+import (
+    "context"
+    "fmt"
+    "log"
+    "net"
+
+    proto "github.com/Costin2000/GoChat---Schwarz-Internship---2025/services/friend-request-base/proto"
+    "google.golang.org/grpc"
+)
+
+type friendRequestServer struct {
+    proto.UnimplementedFriendRequestServiceServer
+}
+
+func (s *friendRequestServer) Ping(ctx context.Context, in *proto.Empty) (*proto.Pong, error) {
+    return &proto.Pong{Message: "pong from friend-request-base"}, nil
+}
 
 func main() {
+    lis, err := net.Listen("tcp", ":50052")
+    if err != nil {
+        log.Fatalf("failed to listen: %v", err)
+    }
 
+    grpcServer := grpc.NewServer()
+    proto.RegisterFriendRequestServiceServer(grpcServer, &friendRequestServer{})
+
+    fmt.Println("Friend Request gRPC server listening on :50052...")
+
+    if err := grpcServer.Serve(lis); err != nil {
+        log.Fatalf("failed to serve: %v", err)
+    }
 }
