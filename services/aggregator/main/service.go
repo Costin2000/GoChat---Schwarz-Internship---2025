c437284f291fa0e653aa483c2196a21f1de7f86b
Dimitrie Tanasescu <dimi.tanasescu@yahoo.com>
2025-09-05 18:34:39 +0300

    GI23-178 setup aggregator service

---
+++ services/aggregator/main/service.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"bufio"
+	"database/sql"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"path/filepath"
+	"strings"
+
+	aggrpb "github.com/Costin2000/GoChat---Schwarz-Internship---2025/services/aggregator/proto"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const port = ":50054"
+
+type AggregatorService struct {
+	storageAccess StorageAccess
+	aggrpb.UnimplementedAggregatorServiceServer
+}
+
+// retrieve db setup from the .env file
+func loadEnv(filename string) error {
+
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		// ignore empty lines and comments
+		if len(line) == 0 || strings.HasPrefix(line, "#") {
+			continue
+		}
+
+		envs := strings.SplitN(line, "=", 2)
+		if len(envs) != 2 {
+			continue // skip bad lines
+		}
+
+		key := strings.TrimSpace(envs[0])
+		value := strings.TrimSpace(envs[1])
+
+		// Set the environment variable
+		if err := os.Setenv(key, value); err != nil {
+			log.Printf("Warning: could not set env var %s: %v", key, err)
+		}
+	}
+
+	return scanner.Err()
+}
+
+func main() {
+
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("Could not get current working directory: %v", err)
+	}
+
+	envPath := "./db/.env" // db env path from root
+	if filepath.Base(wd) == "aggregator" {
+		envPath = "./../../db/.env" // db env path from user-base service directory
+	}
+
+	// db connection
+	if err := loadEnv(envPath); err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	dbUser := os.Getenv("POSTGRES_USER")
+	dbPassword := os.Getenv("POSTGRES_PASSWORD")
+	dbName := os.Getenv("POSTGRES_DB")
+	dbPort := os.Getenv("DB_PORT")
+
+	var dbHost string
+	if os.Getenv("ENV") == "docker" {
+		dbHost = "postgres-db"
+	} else {
+		dbHost = os.Getenv("DB_HOST")
+	}
+
+	connStr := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable", dbUser, dbPassword, dbHost, dbPort, dbName)
+	db, err := sql.Open("pgx", connStr)
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatalf("Failed to ping database: %v", err)
+	}
+	log.Println("Successfully connected to PostgreSQL database.")
+
+	// network connection
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Error polling port %s %v", port, err)
+	}
+
+	// server connections
+	storage := newPostgresAccess(db)
+	aggregator := &AggregatorService{
+		storageAccess: storage,
+	}
+
+	grpcServer := grpc.NewServer()
+
+	aggrpb.RegisterAggregatorServiceServer(grpcServer, aggregator)
+	reflection.Register(grpcServer)
+
+	log.Printf("gRPC polling on port %s...", port)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
+}
