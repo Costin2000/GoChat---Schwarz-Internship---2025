1952d85d1252d750f5115a3bfcbcfa2fc40e7621
Alexandru Axenia <94038640+AlexAxenia@users.noreply.github.com>
2025-09-12 11:14:03 +0300

    Update init.sql

--- devtest-db/init.sql
+++ devtest-db/init.sql
@@ -1,14 +1,19 @@
+BEGIN;
+
 -- Testing database - every run is a clean run to ensure safety when introducing or removing dummy data
 
 -- drop tables and types in order of dependency to avoid foreign key constraint errors
+
 DROP TABLE IF EXISTS "Message";
-DROP TABLE IF EXISTS "FriendRequest";
+DROP TABLE IF EXISTS "Friend Requests";
 DROP TABLE IF EXISTS "Conversation";
 DROP TABLE IF EXISTS "User";
+
 DROP TYPE IF EXISTS FRIEND_REQUEST_STATUS;
 
+
 CREATE TABLE IF NOT EXISTS "User" (
-    id BIGSERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY, -- BIGSERIAL instead of TEXT to ensure automatic id increment without race conditions
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     user_name TEXT NOT NULL UNIQUE,
@@ -19,17 +24,18 @@ CREATE TABLE IF NOT EXISTS "User" (
 
 CREATE TYPE FRIEND_REQUEST_STATUS AS ENUM ('pending', 'accepted', 'rejected', 'blocked');
 
-CREATE TABLE IF NOT EXISTS "FriendRequest" (
+CREATE TABLE IF NOT EXISTS "Friend Requests" (
     id BIGSERIAL PRIMARY KEY,
     sender_id BIGINT NOT NULL REFERENCES "User"(id) ON DELETE CASCADE,
     receiver_id BIGINT NOT NULL REFERENCES "User"(id) ON DELETE CASCADE,
     status FRIEND_REQUEST_STATUS NOT NULL DEFAULT 'pending',
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+
     CHECK (sender_id <> receiver_id)
 );
 
 CREATE UNIQUE INDEX IF NOT EXISTS FRIEND_REQUEST_ORDER_IDX 
-ON "FriendRequest" (LEAST(sender_id, receiver_id), GREATEST(sender_id, receiver_id));
+ON "Friend Requests" (LEAST(sender_id, receiver_id), GREATEST(sender_id, receiver_id));
 
 CREATE TABLE IF NOT EXISTS "Conversation" (
     id BIGSERIAL PRIMARY KEY,
@@ -37,6 +43,7 @@ CREATE TABLE IF NOT EXISTS "Conversation" (
     user2_id BIGINT NOT NULL REFERENCES "User"(id) ON DELETE CASCADE,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+
     CHECK (user1_id <> user2_id)
 );
 
@@ -53,3 +60,5 @@ CREATE TABLE IF NOT EXISTS "Message" (
 
 CREATE INDEX IF NOT EXISTS message_conv_created_idx 
 ON "Message"(conversation_id, created_at);
+
+COMMIT;
