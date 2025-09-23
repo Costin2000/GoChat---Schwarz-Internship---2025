b182e6092b22ed3749ee711ee8bdf9e3beeba3c7
Alexandru Axenia <axeniaalex1@yahoo.com>
2025-09-23 10:24:47 +0300

    Friend request (create and accept) mail

--- services/friend-request-base/main/create-friend-request.go
+++ services/friend-request-base/main/create-friend-request.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"strconv"
 
 	proto "github.com/Costin2000/GoChat---Schwarz-Internship---2025/services/friend-request-base/proto"
+	pbuser "github.com/Costin2000/GoChat---Schwarz-Internship---2025/services/user-base/proto"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -30,5 +33,66 @@ func (svc *friendRequestService) CreateFriendRequest(ctx context.Context, req *p
 		return nil, err
 	}
 
+	// Trimitem email doar daca avem publisher si clientul user-base
+	if svc.emailPub != nil && svc.userClient != nil {
+		receiver, err := svc.getUserByID(ctx, receiverID)
+		if err != nil {
+			log.Printf("WARN: could not fetch receiver user %s: %v", receiverID, err)
+		} else {
+			sender, sErr := svc.getUserByID(ctx, senderID)
+			if sErr != nil {
+				log.Printf("WARN: could not fetch sender user %s: %v", senderID, sErr)
+			}
+
+			senderName := ""
+			senderUsername := ""
+			if sender != nil {
+				senderName = fmt.Sprintf("%s %s", sender.FirstName, sender.LastName)
+				senderUsername = sender.UserName
+			}
+
+			subject := "You received a friend request!"
+			body := fmt.Sprintf("Hi %s,\n\nYou have a new friend request from %s (@%s).\n\n- GoChat Team",
+				receiver.FirstName, senderName, senderUsername,
+			)
+
+			if pubErr := svc.emailPub.Publish(ctx, EmailMessage{
+				To:      receiver.Email,
+				Subject: subject,
+				Body:    body,
+			}); pubErr != nil {
+				log.Printf("WARN: failed to publish email for friend request to %s: %v", receiver.Email, pubErr)
+			}
+		}
+	}
+
 	return friendRequestResp, nil
+}
+
+// helper: obtine user din user-base folosind ListUsers + filter by id
+func (svc *friendRequestService) getUserByID(ctx context.Context, idStr string) (*pbuser.User, error) {
+	id, err := strconv.ParseInt(idStr, 10, 64)
+	if err != nil {
+		return nil, fmt.Errorf("invalid user id %s: %w", idStr, err)
+	}
+
+	resp, err := svc.userClient.ListUsers(ctx, &pbuser.ListUsersRequest{
+		PageSize: 1,
+		Filters: []*pbuser.ListUsersFiltersOneOf{
+			{
+				Filter: &pbuser.ListUsersFiltersOneOf_UserIds{
+					UserIds: &pbuser.FilterByIdIn{
+						UserId: []int64{id},
+					},
+				},
+			},
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	if len(resp.Users) == 0 {
+		return nil, status.Errorf(codes.NotFound, "user with id %s not found", idStr)
+	}
+	return resp.Users[0], nil
 }
